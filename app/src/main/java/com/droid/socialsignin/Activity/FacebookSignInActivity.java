74e07c96ffd93fafe3066c599a49207d00faf1fc
Nightchar <nikhilomar.bhumca08@gmail.com>
2017-05-23 18:18:26 +0530

    First draft

---
+++ app/src/main/java/com/droid/socialsignin/Activity/FacebookSignInActivity.java
@@ -0,0 +1,10 @@
+package com.droid.socialsignin.Activity;
+
+import android.app.Activity;
+
+/**
+ * Created by nikhil1804 on 23-05-2017.
+ */
+
+public class FacebookSignInActivity extends Activity {
+}
---
+++ app/src/main/java/com/droid/socialsignin/Activity/SocialSignInActivity.java
@@ -0,0 +1,36 @@
+package com.droid.socialsignin.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.droid.socialsignin.R;
+
+public class SocialSignInActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_sign_in);
+        findViewById(R.id.btn_google_sign_in).setOnClickListener(this);
+        findViewById(R.id.btn_googl_plus_sign_in).setOnClickListener(this);
+        findViewById(R.id.btn_facebook_sign_in).setOnClickListener(this);
+        findViewById(R.id.btn_twitter_sign_in).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_google_sign_in:
+                startActivity(new Intent(SocialSignInActivity.this, GoogleSignInActivity.class));
+                break;
+            case R.id.btn_googl_plus_sign_in:
+                break;
+            case R.id.btn_facebook_sign_in:
+                break;
+            case R.id.btn_twitter_sign_in:
+                break;
+        }
+    }
+}
